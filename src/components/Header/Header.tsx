ec297d11188fccb4b42c62a78cdf3ea5d1324247
Alex Uribe <repzyyog@gmail.com>
2025-10-27 13:05:42 +0100

    Create Header component with styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import type React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="main-header">
+      <div className="main-header_container">
+        <h1 className="main-header_title">🔴Redpointt</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
